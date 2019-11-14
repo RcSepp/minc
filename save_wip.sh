7e201487ac5619ffa5c1c57722e1914af3783794
Klaassen, Sebastian <klaassens@ornl.gov>
2019-11-13 20:00:16 -0800

    Fix stash conflicts

--- save_wip.sh
+++ save_wip.sh
@@ -1,11 +1,11 @@
 current_branch=$(git branch 2>/dev/null | grep "^*" | cut -c3-)
 git stash -u
 git checkout wip
-git reset --hard
+git pull
 git merge -X theirs $current_branch --no-commit --no-ff
 rm .git/MERGE_HEAD
-git stash pop
-git add -A
+git checkout stash -- .
 git commit -m "$(date +'%Y/%m/%d')"
 git checkout $current_branch
 git push --all
+git stash pop

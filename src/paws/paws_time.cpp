aa1bbe2a69c1bff9a06f2d1646d46e1b254dd3d7
RcSepp <rcsepp@hotmail.com>
2020-08-07 21:54:35 -0700

    Add PawsDuration::toString()

--- src/paws/paws_time.cpp
+++ src/paws/paws_time.cpp
@@ -4,9 +4,73 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <sstream>
 
 typedef std::chrono::duration<double> duration;
 typedef PawsValue<duration> PawsDuration;
+
+template<> const std::string PawsDuration::toString() const
+{
+	duration input_seconds = val;
+
+	auto h = std::chrono::duration_cast<std::chrono::hours>(input_seconds);
+	input_seconds -= h;
+	auto m = std::chrono::duration_cast<std::chrono::minutes>(input_seconds);
+	input_seconds -= m;
+	auto s = std::chrono::duration_cast<std::chrono::seconds>(input_seconds);
+	input_seconds -= s;
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(input_seconds);
+	input_seconds -= ms;
+	auto us = std::chrono::duration_cast<std::chrono::microseconds>(input_seconds);
+	input_seconds -= us;
+	auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(input_seconds);
+
+	auto hc = h.count(), mc = m.count(), sc = s.count(), msc = ms.count(), usc = us.count(), nsc = ns.count();
+	uint8_t mask = (!!hc) << 0 | (!!mc) << 1 | (!!sc) << 2 | (!!msc) << 3 | (!!usc) << 4 | (!!nsc) << 5;
+
+	if (mask == 0x0)
+		return "0 nanoseconds";
+
+	std::stringstream ss;
+
+	if (hc)
+		ss << hc << (hc == 1 ? " hour" : " hours");
+
+	if (mc)
+	{
+		if (mask & 0x1) ss << (mask & ~0x3 ? ", " : " and ");
+		ss << mc << (mc == 1 ? " minute" : " minutes");
+	}
+
+	if (sc)
+	{
+		if (mask & 0x3) ss << (mask & ~0x7 ? ", " : " and ");
+		ss << sc << (sc == 1 ? " second" : " seconds");
+	}
+
+	if (msc)
+	{
+		if (mask & 0x7) ss << (mask & ~0xF ? ", " : " and ");
+		ss << msc << (msc == 1 ? " millisecond" : " milliseconds");
+	}
+
+	if (usc)
+	{
+		if (mask & 0xF) ss << (mask & ~0x1F ? ", " : " and ");
+		ss << usc << (usc == 1 ? " microsecond" : " microseconds");
+	}
+
+	if (nsc)
+	{
+		if (mask & 0x1F) ss << (mask & ~0x3F ? ", " : " and ");
+		ss << nsc << (nsc == 1 ? " nanosecond" : " nanoseconds");
+	}
+
+	//TODO: Figure out better logic on how to humanize durations
+	//		i.e. "1 hour and 15 minutes" vs. "75 minutes", "892.6 milliseconds" vs. "0.8926 seconds" vs. "892 milliseconds and 600 microseconds", ...
+
+	return ss.str();
+}
 
 MincPackage PAWS_TIME("paws.time", [](MincBlockExpr* pkgScope) {
 	registerType<PawsDuration>(pkgScope, "PawsDuration");
@@ -18,6 +82,8 @@ MincPackage PAWS_TIME("paws.time", [](MincBlockExpr* pkgScope) {
 	defineExpr(pkgScope, "$E<PawsDuration>.milliseconds", +[](duration d) -> int { return std::chrono::duration_cast<std::chrono::milliseconds>(d).count(); } );
 	defineExpr(pkgScope, "$E<PawsDuration>.microseconds", +[](duration d) -> int { return std::chrono::duration_cast<std::chrono::microseconds>(d).count(); } );
 	defineExpr(pkgScope, "$E<PawsDuration>.nanoseconds", +[](duration d) -> int { return std::chrono::duration_cast<std::chrono::nanoseconds>(d).count(); } );
+
+	//TODO: Define setters
 
 	// Define PawsDuration operators
 	defineExpr(pkgScope, "$E<PawsDuration> + $E<PawsDuration>", +[](duration d0, duration d1) -> duration { return d0 + d1; } );
